97e5ae7c7b5fc868777027031d42dcaff75f50fc
Matt Pruitt <guitsaru@gmail.com>
2020-09-20 22:40:36 +0700

    Releases

--- config/releases.exs
+++ config/releases.exs
@@ -23,7 +23,7 @@ config :website, WebsiteWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :website, WebsiteWeb.Endpoint, server: true
+config :website, WebsiteWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
